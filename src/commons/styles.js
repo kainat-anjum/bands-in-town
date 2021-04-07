5df67439313fcfe648498753f08fe32ab2643233
kainat-anjum <kainat.anjum@emumba.com>
2021-04-07 18:54:15 +0500

    adds common assets

---
+++ src/commons/styles.js
@@ -0,0 +1,5 @@
+import {css} from "styled-components";
+
+export const ContainerPadding = css`
+  padding: 0 50px;
+`;
---
+++ src/commons/variables.js
@@ -0,0 +1,30 @@
+export const size = {
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "425px",
+  tablet: "768px",
+  laptop: "1024px",
+  laptopL: "1440px",
+  desktop: "2560px",
+};
+
+export const device = {
+  mobileS: `(max-width: ${size.mobileS})`,
+  mobileM: `(max-width: ${size.mobileM})`,
+  mobileL: `(max-width: ${size.mobileL})`,
+  tablet: `(max-width: ${size.tablet})`,
+  laptop: `(max-width: ${size.laptop})`,
+  laptopL: `(max-width: ${size.laptopL})`,
+  desktop: `(max-width: ${size.desktop})`,
+  desktopL: `(max-width: ${size.desktop})`,
+};
+
+export const colors = {
+  primaryColor: "#13182b",
+  secondaryColor: "#3bc8e6",
+  darkBlueColor: "#1b1f38",
+  whiteColor: "#fff",
+  grayColor: "#84859c",
+  alphaGrayColor: "#2c3046",
+  betaGrayColor: "#656b6b",
+};
