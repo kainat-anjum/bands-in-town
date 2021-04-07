5f76d07823c6c9b3c712f11423fe43245848545d
kainat-anjum <kainat.anjum@emumba.com>
2021-04-07 18:55:10 +0500

    adds css style resets and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import './App.css';
+import "./App.scss";
+import React, { useReducer, useMemo } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { reducer } from "./reducers/reducers";
+
+import { Home } from "./pages/Home/Home";
+import { Artist } from "./pages/Artist/Artist";
+import { ArtistContext } from "./context/ArtistContext";
+import { initialState } from "./meta";
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const value = useMemo(() => ({ state, dispatch }), [state, dispatch]);
+  
   return (
     <div className="App">
+      <ArtistContext.Provider value={value}>
+        <Router>
+          <Route path="/" exact component={Home} />
+          <Route path="/artist" exact component={Artist} />
+        </Router>
+      </ArtistContext.Provider>
     </div>
   );
 }
